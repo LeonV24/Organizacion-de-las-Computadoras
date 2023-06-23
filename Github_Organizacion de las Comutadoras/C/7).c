bde6938d79e846111d49012d0591bdfdfd7018f1
Pedro <75513002+P3-dr0@users.noreply.github.com>
2023-06-23 20:33:24 -0300

    Create 7).c

---
+++ Github_Organizacion de las Comutadoras/C/7).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero, numero1;
+    printf("Ingrese un numero: ");
+    scanf("%d" , &numero);
+
+    printf("Ingrese un numero: ");
+    scanf("%d" , &numero1);
+
+    printf("El primer numero es %d%d\n", numero, numero1) ;
+    printf("El segundo numero es %d%d\n", numero1, numero) ;
+}
