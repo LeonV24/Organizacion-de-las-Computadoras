c9e46ed843132108b293394ad708664a85ab934f
Pedro <75513002+P3-dr0@users.noreply.github.com>
2023-06-30 20:40:50 -0300

    Create 2).c

---
+++ Github_Organizacion de las Comutadoras/C/Practica N1/2).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int d, m, a;
+    printf("Ingrese el dia: ");
+    scanf("%d", &d);
+    printf("Ingrese el mes: ");
+    scanf("%d", &m);
+    printf("Ingrese el año: ");
+    scanf("%d", &a);
+
+    printf("La fecha queda asi %d/%d/%d ", d, m, a);
+}
